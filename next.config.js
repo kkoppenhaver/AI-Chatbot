cf75d57eda6a36ca85d3571fee7a26459b98f728
Keanan Koppenhaver <keanan@alphaparticle.com>
2024-01-14 16:57:41 -0600

    Update externals calls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    })
-    return config
-  },
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    webpack: (config) => {
+        config.externals.push({
+	      'utf-8-validate': 'commonjs utf-8-validate',
+	      'bufferutil': 'commonjs bufferutil',
+	    })
+
+        config.externals = [...config.externals, "hnswlib-node"]
+
+        return config
+    },
 }
+
+export default nextConfig
